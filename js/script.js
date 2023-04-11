f5b6559a4ff37d0ad2e92cb32d6ede35f23669d2
José Walisson <josewalisonpessoadelima@gmail.com>
2023-04-11 11:18:22 -0300

    Arrumando botões, colocando botão de fechar e ajustando tamanho do icon

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 // Fecha e abre o menu
 document.addEventListener('DOMContentLoaded', function() {
-    const menu = document.querySelector('#icon-container span');
-    const buttons = document.querySelectorAll('.buttons button');
-  
-    menu.addEventListener('click', function() {
-      buttons.forEach(button => {
-        if (button.style.display === 'none') {
-          button.style.display = 'block';
-        } else {
-          button.style.display = 'none';
-        }
-      });
+  const menu = document.querySelector('#icon-container');
+  const buttons = document.querySelectorAll('.buttons button');
+  const iconAbrir = document.querySelector('#icon-container > span');
+  const iconFechar = document.querySelector('#icon-container .fechar span');
+
+  menu.addEventListener('click', function() {
+    buttons.forEach(button => {
+      if (button.style.display === 'none') {
+        button.style.display = 'block';
+        iconAbrir.style.display = 'none';
+        iconFechar.style.display = 'block';
+      } else {
+        button.style.display = 'none';
+        iconAbrir.style.display = 'block';
+        iconFechar.style.display = 'none';
+      }
     });
   });
+});
